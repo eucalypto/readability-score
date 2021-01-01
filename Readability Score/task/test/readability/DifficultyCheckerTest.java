9fb8da73e63faa367a28bbd618443d888c114ee6
Viktor Papara <developer@eucalypto.net>
2021-01-01 18:12:57 +0100

    Restructure DifficultyCheckerTest
    by using JUnit5's @Nested and @DisplayName to get beautiful and plain test results.

--- Readability Score/task/test/readability/DifficultyCheckerTest.java
+++ Readability Score/task/test/readability/DifficultyCheckerTest.java
@@ -1,10 +1,13 @@
 package readability;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Difficulty Checker")
 class DifficultyCheckerTest {
 
     DifficultyChecker checker;
@@ -14,34 +17,51 @@ class DifficultyCheckerTest {
         checker = new DifficultyChecker();
     }
 
-    @Test
-    void simpleCheckLessThan100Chars() {
-        var simple = "This is simple to read!";
+    @Nested
+    @DisplayName("Simple Check")
+    class SimpleCheck {
 
-        var result = checker.simpleCheck(simple);
+        @Nested
+        @DisplayName("outputs HARD")
+        class OutputsHard {
 
-        assertEquals(Difficulty.EASY, result);
-    }
+            @Test
+            @DisplayName("at over 100 Characters")
+            void over100Chars() {
+                var hard = "This text is hard to read. It contains a lot of sentences as well as a lot of words in each sentence.";
 
-    @Test
-    void simpleCheckOver100Chars() {
-        var hard = "This text is hard to read. It contains a lot of sentences as well as a lot of words in each sentence.";
+                var result = checker.simpleCheck(hard);
 
-        var result = checker.simpleCheck(hard);
-
-        assertEquals(Difficulty.HARD, result);
-    }
-
-    @Test
-    void simpleCheckExactly100Chars() {
-        StringBuilder exact = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            exact.append("1234567890");
+                assertEquals(Difficulty.HARD, result);
+            }
         }
 
-        var result = checker.simpleCheck(exact.toString());
+        @Nested
+        @DisplayName("outputs EASY")
+        class OutputsEasy {
 
-        assertEquals(Difficulty.EASY, result);
+            @Test
+            @DisplayName("below 100 Characters")
+            void lessThan100Chars() {
+                var simple = "This is simple to read!";
+
+                var result = checker.simpleCheck(simple);
+
+                assertEquals(Difficulty.EASY, result);
+            }
+
+            @Test
+            @DisplayName("at exactly 100 Characters")
+            void exactly100Chars() {
+                var oneHundredCharacters = "1234567890".repeat(10);
+                var result = checker.simpleCheck(oneHundredCharacters);
+
+                assertEquals(Difficulty.EASY, result);
+            }
+
+        }
+
+
     }
 
 }
